b86b379eca9644e573f9d528c5de03a958fde1d5
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-17 10:51:42 +0530

    Program to sum of numbers from 1 to n using recursion

---
+++ src/main/java/com/bootcoding/dsa/recursion/SumOfNumbers.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.recursion;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+         int n =  15;
+        System.out.println(printSum(n));
+    }
+
+    public static int printSum(int n) {
+        if(n<=1) {
+            return n;
+        }
+            return n +printSum(n-1);
+        }
+    }
+
