d439763c3964f848daa24585c1e3c7fee2178347
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-17 11:50:10 +0530

    Program of linked list

--- src/main/java/com/bootcoding/dsa/linkedlist/Node.java
+++ src/main/java/com/bootcoding/dsa/linkedlist/Node.java
@@ -6,12 +6,14 @@ import java.util.LinkedList;
 public class Node {
     int data;
     Node next;
-    public Node(int data){
-        this.data=data;
-        this.next=null;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
     }
+
     //creating linkedlist
-    public static Node createLinkedList(){
+    public static Node createLinkedList() {
         Node firstNode = new Node(10);
         Node secondNode = new Node(20);
         Node thirdNode = new Node(30);
@@ -24,19 +26,44 @@ public class Node {
         fourthNode.next = null;
         return head;
     }
-   //printing linkedlist
-    public static void printLinkedList(Node head)
-    {
-        Node temp = head;
-        while (temp != null){
-            System.out.println(temp.data);
-            temp = temp.next;
-        }
-    }
-   
-    public static void main(String[] args) {
-        createLinkedList();
-        printLinkedList(createLinkedList());
+
+    public static Node insertNodeAtFirst(Node head) {
+        Node newNode = new Node(10);
+        newNode.next = head;
+        head = newNode;
+        return head;
     }
 
+    public static Node insertNodeByLast(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.next == null) {
+                Node newNode = new Node(60);
+                temp.next = newNode;
+                return head;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static void printLinkedList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        Node head = createLinkedList();
+        printLinkedList(head);
+        insertNodeAtFirst(head);
+        printLinkedList(insertNodeAtFirst(head));
+        insertNodeByLast(head);
+        printLinkedList(insertNodeByLast(head));
+
+    }
 }
