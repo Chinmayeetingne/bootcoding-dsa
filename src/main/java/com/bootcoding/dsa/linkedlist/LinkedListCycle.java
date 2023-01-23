80fd7b4213b9f731e9782e817c7b77ecd365cd7c
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-23 09:49:34 +0530

    Program for linked list cycle

---
+++ src/main/java/com/bootcoding/dsa/linkedlist/LinkedListCycle.java
@@ -0,0 +1,36 @@
+package com.bootcoding.dsa.linkedlist;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+
+
+        Node firstNode = new Node(3);
+        Node secondNode = new Node(2);
+        Node thirdNode = new Node(0);
+        Node fourthNode = new Node(-4);
+
+
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = fourthNode;
+        fourthNode.next = secondNode;
+       // return firstNode;
+
+        System.out.println(hasCycle(head));
+    }
+
+    public static boolean hasCycle(Node head) {
+        Node sp = head;
+        Node fp = head;
+        while(fp != null && fp.next != null){
+            sp =sp.next;
+            fp =fp.next.next;
+            if(sp == fp){
+                return true;
+            }
+        }
+        return false;
+    }
+}
