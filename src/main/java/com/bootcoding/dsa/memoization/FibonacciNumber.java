9854664ffec965f08a05b7a9dff3e3e9089f59cf
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-25 09:21:57 +0530

    Program for fibonacci number using memoization

---
+++ src/main/java/com/bootcoding/dsa/memoization/FibonacciNumber.java
@@ -0,0 +1,30 @@
+package com.bootcoding.dsa.memoization;
+
+import java.util.HashMap;
+
+public class FibonacciNumber {
+    static HashMap<Integer , Integer> cache = new HashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+        System.out.println(fibMemoization(5));
+    }
+    public static int fib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+
+    public static int fibMemoization(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        if (cache.containsKey(n)){
+            return cache.get(n);
+        }
+        cache.put(n,fibMemoization(n-1)+ fibMemoization(n-2));
+        return cache.get(n);
+    }
+}
