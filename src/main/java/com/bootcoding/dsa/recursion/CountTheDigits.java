2864d2b8e57bc1bf2005d610b6ac740d8cc6037b
Chinmayeetingne <rashitingne12@gmail.com>
2023-01-30 20:02:17 +0530

    Program for array

--- src/main/java/com/bootcoding/dsa/recursion/CountTheDigits.java
+++ src/main/java/com/bootcoding/dsa/recursion/CountTheDigits.java
@@ -2,15 +2,16 @@ package com.bootcoding.dsa.recursion;
 
   public class CountTheDigits {
     public static void main(String[] args) {
-       int n = 234;
+       int n = 99;
         System.out.println("Number of digits : " + countDigit(n));
     }
 
     public static int countDigit(int n){
         {
-            if (n/10 == 0)
+            if (n <10)
                 return 1;
             return 1 + countDigit(n / 10);
+
         }
     }
 }
