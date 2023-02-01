347b9aeba45597368ca96467cc36c69acc7e4aec
Chinmayeetingne <rashitingne12@gmail.com>
2023-02-01 14:59:24 +0530

    Programs on binary tree

---
+++ src/main/java/com/bootcoding/dsa/tree/TreeNode.java
@@ -0,0 +1,13 @@
+package com.bootcoding.dsa.tree;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data){
+        this.data = data;
+        this.left= null;
+        this.right= null;
+    }
+}
